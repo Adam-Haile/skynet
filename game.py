b2516cfeb90cca22cb1ace91d243c1908a981b66
DevArtech <iamhaile222@gmail.com>
2023-04-02 20:05:28 -0500

    Local 1v1 Game
    Establish base code to allow a 1v1 game to occur with GUI

--- game.py
+++ game.py
@@ -71,7 +71,7 @@ def drawDeck():
     return cards.pop()
 
 def drawDiscard():
-    return discardedCards.pop()
+    return discardedCards.pop(0)
 
 def discard(card):
     discardedCards.insert(0, card)
@@ -102,35 +102,26 @@ def isGameOver():
 
     return gameOver
 
-def tallyRoundScore():
-    if isRoundOver() is not 0:
-        global playerOne
-        global playerTwo
-        global playerOneVisible
-        global playerTwoVisible
-        
-        lastWinner = isRoundOver()
-        playerOneScore = getPlayerScore(1)
-        playerTwoScore = getPlayerScore(2)
+def swapCard(player, i, value):
+    if player == 1:
+        playerOneTotal[i] = value
+        playerOneVisible[i] = value
+    if player == 2:
+        playerTwoTotal[i] = value
+        playerTwoVisible[i] = value
 
-        if lastWinner == 1 and playerOneScore <= playerTwoScore:
-            playerOneScore *= 2
-        if lastWinner == 2 and playerTwoScore <= playerOneScore:
-            playerTwoScore *= 2
+def switchCurrent():
+    global currentPlayer
 
-        playerOne += playerOneScore
-        playerTwo += playerTwoScore
-        cards.clear()
-        discardedCards.clear()
-        playerOneTotal.clear()
-        playerOneVisible.clear()
-        playerOneVisible = [None] * 12
-        playerTwoTotal.clear()
-        playerTwoVisible.clear()
-        playerTwoVisible = [None] * 12
-
+    if currentPlayer == 1:
+        currentPlayer = 2
+    elif currentPlayer == 2:
+        currentPlayer = 1
 
 running = True
+
+newRound()
+
 
 if running:
     pygame.init()
@@ -150,11 +141,11 @@ YELLOW = (255, 255, 100)
 RED = (255, 100, 100)
 font = pygame.font.Font(None, 75)
 skyfont = pygame.font.Font(None, 40)
+smallfont = pygame.font.Font(None, 22)
 
-newRound()
 
 class Card():
-    def __init__(self, value, position):
+    def __init__(self, value, position=0):
         super().__init__()
         self.image = pygame.Surface((card_width, card_height))
         self.rect = self.image.get_rect()
@@ -201,6 +192,55 @@ class Card():
     def getRect(self):
         return self.rect
     
+swapFromDeck = False
+swapFromDiscard = False
+drawnCard = -3
+lastSwap = False
+roundOver = False
+topDiscard = Card(None)
+
+def tallyRoundScore():
+    if isRoundOver() is not 0:
+        global playerOne
+        global playerTwo
+        global playerOneVisible
+        global playerTwoVisible
+        
+        lastWinner = isRoundOver()
+        playerOneScore = getPlayerScore(1)
+        playerTwoScore = getPlayerScore(2)
+
+        if lastWinner == 1 and playerOneScore <= playerTwoScore:
+            playerOneScore *= 2
+        if lastWinner == 2 and playerTwoScore <= playerOneScore:
+            playerTwoScore *= 2
+
+        playerOne += playerOneScore
+        playerTwo += playerTwoScore
+        cards.clear()
+        discardedCards.clear()
+        playerOneTotal.clear()
+        playerOneVisible.clear()
+        playerOneVisible = [None] * 12
+        playerTwoTotal.clear()
+        playerTwoVisible.clear()
+        playerTwoVisible = [None] * 12
+        if running:
+            global swapFromDeck
+            global swapFromDiscard
+            global drawnCard
+            global lastSwap
+            global roundOver
+            global topDiscard
+            swapFromDeck = False
+            swapFromDiscard = False
+            drawnCard = -3
+            lastSwap = False
+            roundOver = False
+            topDiscard = Card(None)
+
+        newRound()
+
 while running:
     playerOneGameCards = []
     playerTwoGameCards = []
@@ -212,33 +252,102 @@ while running:
     window.fill((95, 141, 186))
 
     i = 0
+
+    p1 = None
+    p2 = None
+    if getPlayerScore(1) < getPlayerScore(2):
+        p1 = GREEN
+        p2 = RED
+    elif getPlayerScore(2) < getPlayerScore(1):
+        p1 = RED
+        p2 = GREEN
+    else:
+        p1 = YELLOW
+        p2 = YELLOW
+
+    text = skyfont.render("Current Player: " + str(currentPlayer), True, (255, 255, 255))
+    window.blit(text, [boundsX/2 - 125, boundsY - 75, text.get_rect().width, text.get_rect().height])
+
+    text = smallfont.render("Total Player Score: " + str(playerOne), True, (255, 255, 255))
+    window.blit(text, [boundsX/20, card_height/2 - 35, text.get_rect().width, text.get_rect().height])
+
+    text = skyfont.render("Player Score: " + str(getPlayerScore(1)), True, p1)
+    window.blit(text, [boundsX/20, card_height/2 - 15, text.get_rect().width, text.get_rect().height])
+
+    text = smallfont.render("Total Player Score: " + str(playerTwo), True, (255, 255, 255))
+    window.blit(text, [boundsX/20 + 500, card_height/2 - 35, text.get_rect().width, text.get_rect().height])
+
+    text = skyfont.render("Player Score: " + str(getPlayerScore(2)), True, p2)
+    window.blit(text, [boundsX/20 + 500, card_height/2 - 15, text.get_rect().width, text.get_rect().height])
+
     for y in range(3):
         for x in range(4):
             newCard = Card(playerOneVisible[i], i)
-            newCard.moveTo((boundsX / 10) + (x * 105), (boundsY / 4) + (y * 200))
+            newCard.moveTo((boundsX / 10) + (x * 105), (boundsY / 4) + (y * 200) - 25)
             newCard.draw(window)
             playerOneGameCards.append(newCard)
 
             newCard = Card(playerTwoVisible[i], i)
-            newCard.moveTo((boundsX / 10) + (x * 105) + 500, (boundsY / 4) + (y * 200))
+            newCard.moveTo((boundsX / 10) + (x * 105) + 500, (boundsY / 4) + (y * 200) - 25)
             newCard.draw(window)
             playerTwoGameCards.append(newCard)
             i += 1
 
+    if len(discardedCards) > 0:
+        topDiscard = Card(discardedCards[0])
+        topDiscard.moveTo((boundsX / 10) + 650, boundsY - 25)
+        topDiscard.draw(window)
+
+    if drawnCard != -3:
+        cardDrawn = Card(drawnCard)
+        cardDrawn.moveTo((boundsX / 10) + 755, boundsY - 25)
+        cardDrawn.draw(window)
+
+    deck = Card(None)
+    deck.moveTo((boundsX / 10) + 860, boundsY - 25)
+    deck.draw(window)
+
     if event.type == pygame.MOUSEBUTTONDOWN:
         mouse_pos = pygame.mouse.get_pos()
-        for card in playerOneGameCards:
-            if card.getRect().collidepoint(mouse_pos):
-                i = card.position
-                playerOneVisible[i] = playerOneTotal[i]
-                
-        for card in playerTwoGameCards:
-            if card.getRect().collidepoint(mouse_pos):
-                i = card.position
-                playerTwoVisible[i] = playerTwoTotal[i]
-                
+
+        if currentPlayer == 1:
+            for card in playerOneGameCards:
+                if card.getRect().collidepoint(mouse_pos):
+                    if swapFromDeck is True or swapFromDiscard is True:
+                        switchCurrent()
+                        discard(playerOneTotal[card.position])
+                        swapCard(1, card.position, drawnCard)
+                        drawnCard = -3
+                        swapFromDeck = False
+                        swapFromDiscard = False
+                        if lastSwap:
+                            roundOver = True
+
+        if currentPlayer == 2:   
+            for card in playerTwoGameCards:
+                if card.getRect().collidepoint(mouse_pos):
+                    if swapFromDeck is True or swapFromDiscard is True:
+                        switchCurrent()
+                        discard(playerTwoTotal[card.position])
+                        swapCard(2, card.position, drawnCard)
+                        drawnCard = -3
+                        swapFromDeck = False
+                        swapFromDiscard = False
+                        if lastSwap:
+                            roundOver = True
+
+        if deck.getRect().collidepoint(mouse_pos) and swapFromDiscard is False and drawnCard == -3:
+            swapFromDeck = True
+            drawnCard = drawDeck()
+        
+        if topDiscard.getRect().collidepoint(mouse_pos) and swapFromDeck is False and drawnCard == -3:
+            swapFromDiscard = True
+            drawnCard = drawDiscard()
+
+    if isRoundOver():
+        lastSwap = True
+
+    if roundOver:
+        tallyRoundScore()
 
     pygame.display.flip()
-
-
-# if running:
